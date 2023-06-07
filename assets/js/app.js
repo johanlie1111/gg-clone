086c8fa070f37289aa18cfa946b84265423a2562
johanlie1111 <xuanquang2105@gmail.com>
2023-06-08 06:54:25 +0700

    gg3

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,13 +8,21 @@
 
 const btnMic = document.querySelector(".icon-right")
 const acb = document.querySelector(".number")
+const bar = document.querySelector(".nav-icon")
 
-console.log(acb);
+console.log(bar);
 
 btnMic.onclick=function(){
     btnMic.classList.toggle("red")
     acb.innerHTML="con cac"
 }
+
+bar.onclick=function(){
+    bar.classList.toggle("app")
+    
+}
+
+
 
 
 
