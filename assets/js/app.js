5ab169110aee71b94ea5faee94fe57768adeca94
johanlie1111 <xuanquang2105@gmail.com>
2023-06-08 03:02:24 +0700

    gg2

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+// const obj = {
+//     name: "QUANG",
+//     phone: "123"
+// }
+// console.log(obj)
+
+// const arr= [vip]
+
+const btnMic = document.querySelector(".icon-right")
+const acb = document.querySelector(".number")
+
+console.log(acb);
+
+btnMic.onclick=function(){
+    btnMic.classList.toggle("red")
+    acb.innerHTML="con cac"
+}
+
+
+
+
+
+
+
+
+
